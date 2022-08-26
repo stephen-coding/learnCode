52ccdf7fef878248729f7e50d02f8e0b6dae0b99
longfei.liu <aseit@2025>
2022-08-26 18:53:34 +0800

    下雨没带伞 15 没啥好想法，雨停了，后续回家在写

---
+++ leetcode/src/main/java/com/life/algorithm/LeetCode15.java
@@ -0,0 +1,23 @@
+package com.life.algorithm;
+
+import java.util.List;
+
+public class LeetCode15 {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        int first = 0;
+        int sec = 1;
+        int third = 2;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                for (int k = 0; k < nums.length; k++) {
+
+                }
+            }
+
+        }
+        return null;
+
+    }
+}
