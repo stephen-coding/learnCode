136f0387bc17fc4f70ed281816d1f3af4a227243
longfei.liu <aseit@2025>
2022-08-26 10:42:00 +0800

    优化 每日一题 1464

---
+++ leetcode/src/main/java/com/life/algorithm/LeetCode1464.java
@@ -0,0 +1,44 @@
+package com.life.algorithm;
+
+import java.util.Arrays;
+
+public class LeetCode1464 {
+
+    //2ms
+    public int maxProduct(int[] nums) {
+
+        Arrays.sort(nums);
+        return ((nums[nums.length - 2] - 1) * (nums[nums.length - 1] - 1) );
+
+
+    }
+
+    //0ms
+    public static int maxProducts1(int[] nums) {
+
+        int maxNumber = 0;
+        int sec_max = 0;
+        for (int num : nums) {
+            if (num > maxNumber) {
+                sec_max = maxNumber;
+                maxNumber = num;
+
+            } else {
+                if (num > sec_max) {
+                    sec_max = num;
+
+                }
+            }
+        }
+
+        return (maxNumber - 1) * (sec_max - 1);
+
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 2, 5, 2};
+        int i = maxProducts1(arr);
+        System.out.println(i);
+    }
+}
