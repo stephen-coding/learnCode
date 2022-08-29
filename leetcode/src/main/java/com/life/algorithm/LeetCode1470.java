2561c6f3a4fc7061cd812e3274aceb1e783c190c
longfei <longfe.liui@aseit.cn>
2022-08-29 23:02:50 +0800

    每日一题 easy 1470

--- leetcode/src/main/java/com/life/algorithm/LeetCode1470.java
+++ leetcode/src/main/java/com/life/algorithm/LeetCode1470.java
@@ -1,27 +1,18 @@
 package com.life.algorithm;
 
+
 public class LeetCode1470 {
 
+    //遍历一遍即可
     public int[] shuffle(int[] nums, int n) {
 
-        int prefix = 0;
-        int suffix = n - 1;
-        int[] ints = new int[nums.length];
-
+        int index = 0;
+        int[] ans = new int[nums.length];
         for (int i = 0; i < n; i++) {
-
-            if (i % 2 == 0) {
-                ints[i] = nums[prefix];
-            } else {
-                ints[i] = nums[suffix];
-
-            }
-            prefix++;
-            suffix++;
-
+            ans[index++] = nums[i];
+            ans[index++] = nums[n + i];
         }
-
-        return ints;
+        return ans;
 
 
     }
