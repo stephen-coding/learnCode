46d0969aa9f665dc0543e0984432b896a00b195b
longfei.liu <aseit@2025>
2022-10-13 16:18:58 +0800

    每日一题 medium 769 看了评论区，我有罪

--- leetcode/src/main/java/com/life/algorithm/LeetCode769.java
+++ leetcode/src/main/java/com/life/algorithm/LeetCode769.java
@@ -1,10 +1,23 @@
 package com.life.algorithm;
 
+
+import java.util.Arrays;
+
 public class LeetCode769 {
 
-    public int maxChunksToSorted(int[] arr) {
+    public static int maxChunksToSorted(int[] arr) {
 
-        return 0;
+        //只要找到前i个数中最大的数等于i的，就说明前面的部分是可以升序之后和后面接上的，此时就切一个块出来，以此类推遍历一遍即可
+        int res = 0, max = 0;
+        for (int i = 0; i < arr.length; i++){
+            max = Math.max(max, arr[i]);
+            if (i == max) res++;
+        }
+        return res;
+    }
 
+    public static void main(String[] args) {
+        int[] arr = {1, 0,  2, 3, 4, 5};
+        System.out.println(maxChunksToSorted(arr));
     }
 }
