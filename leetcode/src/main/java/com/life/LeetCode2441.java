0669b41efdadf3ba069617fa95e2927de018bb43
longfei <longfe.liui@aseit.cn>
2023-05-14 13:24:35 +0800

    每日一题 2441 easy

---
+++ leetcode/src/main/java/com/life/LeetCode2441.java
@@ -0,0 +1,36 @@
+package com.life;
+
+public class LeetCode2441 {
+
+    public static int findMaxK(int[] nums) {
+
+        int[] zhengshu = new int[1001];
+        int[] fushu = new int[1001];
+
+        for (int num : nums) {
+            if (num>0){
+                zhengshu[num] = 1;
+            }else {
+                fushu[Math.abs(num)] = -1;
+            }
+        }
+
+        for (int i = 1000; i > 0; i--) {
+            if (zhengshu[i]==0){
+                continue;
+            }
+
+            if (zhengshu[i]+fushu[i]==0){
+                return i;
+            }
+        }
+
+        return -1;
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1,10,6,7,-7,1};
+        findMaxK(nums);
+    }
+}
