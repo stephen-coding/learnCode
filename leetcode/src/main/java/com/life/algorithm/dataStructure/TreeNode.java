11a33cf9646ed5d1bc6c4f4f2637ddfa974dd310
liulongfei <llf@123.com>
2023-09-05 15:52:47 +0800

    2605

--- leetcode/src/main/java/com/life/algorithm/dataStructure/TreeNode.java
+++ leetcode/src/main/java/com/life/algorithm/dataStructure/TreeNode.java
@@ -2,9 +2,9 @@ package com.life.algorithm.dataStructure;
 
 public class TreeNode {
 
-    private int val;
-    private TreeNode left;
-    private TreeNode right;
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
     public TreeNode(){
 
     }
