625d18359b8e0edf0d4eb7710d4eb34c859f23e2
liulongfei <llf@123.com>
2024-02-04 10:19:14 +0800

    292 easy 每日一题

---
+++ leetcode/src/main/java/com/life/algorithm/LeetCode292.java
@@ -0,0 +1,15 @@
+package com.life.algorithm;
+
+/**
+ * @author T596
+ * @date 2024/2/4
+ */
+public class LeetCode292 {
+
+    //巴什博奕（石子问题）
+    public boolean canWinNim(int n) {
+
+        return !(n % 4 == 0);
+
+    }
+}
---
+++ leetcode/src/main/java/com/life/algorithm/LeetCode82.java
@@ -0,0 +1,18 @@
+package com.life.algorithm;
+
+import com.life.algorithm.dataStructure.ListNode;
+
+/**
+ * @author T596
+ * @date 2024/1/15
+ */
+public class LeetCode82 {
+
+    public ListNode deleteDuplicates(ListNode head) {
+
+        ListNode listNode = new ListNode(0, head);
+
+
+        return head;
+    }
+}
